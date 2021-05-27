7721da841a50b48c8f034334b1b672e8185b5a85
Gwavity <72534647+Gwavity@users.noreply.github.com>
2021-05-26 21:35:39 -0400

    Update lightshotGEN.py

--- LightShotGen/lightshotGEN.py
+++ LightShotGen/lightshotGEN.py
@@ -4,11 +4,9 @@ def generateandsendlink(amount):
     chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s --incognito'# Just so your history doesn't get spammed with random screenshot links.
     characters = string.ascii_lowercase + string.digits
     link = random.choice(characters)
-    print(link)
     if link == '0':
         link = random.choice(characters)
     for i in range(amount):
-        print(link)
         if len(link) == 6:
             webbrowser.get(chrome_path).open('https://prnt.sc/' + link)# If you don't care about your history getting spammed with random links, you can remove the "chrome_path" variable above and replace this line with "webbrowser.open('https://prnt.sc/' + link)"
             link = random.choice(characters)
