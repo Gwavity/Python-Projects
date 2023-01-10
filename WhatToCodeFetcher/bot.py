e0dac75ab35e2c73573ad17fa86af72514c040bd
Gwavity <72534647+Gwavity@users.noreply.github.com>
2023-01-10 15:07:47 -0500

    Update bot.py

--- WhatToCodeFetcher/bot.py
+++ WhatToCodeFetcher/bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import scraper
+import os
 
 bot = commands.Bot(command_prefix="$",intents=discord.Intents.all(),case_insensitive=True)
 
@@ -13,9 +14,8 @@ async def getidea(ctx):
     ideaScraper = scraper.Scrape()
     ideaTitle, ideaHeader = ideaScraper.RetreiveCodeIdea()
     embed = discord.Embed(title=ideaTitle,description=ideaHeader,url=ideaScraper.url,color=discord.Color.from_rgb(244,224,77))
-                           #Set these to wherever the images are saved to
-    bulbFile = discord.File("whattocode.png",filename="Lightbulb.png")
-    textFile = discord.File("whattocodetext.png",filename="Text.png")
+    bulbFile = discord.File(f"{os.getcwd()}\whattocodefetcher\whattocode.png",filename="Lightbulb.png")
+    textFile = discord.File(f"{os.getcwd()}\whattocodefetcher\whattocodetext.png",filename="Text.png")
     embed.set_thumbnail(url="attachment://Text.png")
     embed.set_footer(text=ideaScraper.url,icon_url="attachment://Lightbulb.png")
     await ctx.send(files=[bulbFile,textFile],embed=embed)
