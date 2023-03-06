9bdc3285b064afdd3226663c788b28a8cc1b7714
Alexander <72534647+Gwavity@users.noreply.github.com>
2023-03-05 23:48:42 -0500

    Create fasterGetUser.py

---
+++ discord bots/namemcFetcher/fasterGetUser.py
@@ -0,0 +1,21 @@
+import requests
+
+#Faster because instead of scraping the site with beatifulSoup and selenium, it just makes a reuqest to the minecraft API
+
+class getMC:
+    def __init__(self,username):
+        self.url = f"https://api.mojang.com/users/profiles/minecraft/{username}"
+        self.uuid = None
+        self.username = username
+        self.imageLink = None
+    
+    def retrieveIMG(self):
+        return f"https://mc-heads.net/player/{self.username}/128.png"
+    
+    def retrieveUser(self):
+        return requests.get(f"https://api.mojang.com/users/profiles/minecraft/{self.username}")
+    
+    def run(self):
+        user = self.retrieveUser()
+        image = self.retrieveIMG()
+        return user, image
