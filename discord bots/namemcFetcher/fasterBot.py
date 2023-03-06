63ed11fa7128a398772ba894c6f2f3a534a5b36e
Alexander <72534647+Gwavity@users.noreply.github.com>
2023-03-06 00:02:58 -0500

    Update fasterBot.py

--- discord bots/namemcFetcher/fasterBot.py
+++ discord bots/namemcFetcher/fasterBot.py
@@ -16,14 +16,15 @@ async def on_ready():
 async def mc(ctx,username):
     startTime = time.time()
     getUser = getMC(username).run()
-    userJson = getUser[0].json()
-    username = userJson['name']
-    uuid = userJson['id']
-    
+     
     if getUser[0].status_code != 200:
-        embed = discord.Embed(description=f"`Can\'t find user: {username}`",color=discord.Color.from_rgb(0,0,0))
+        embed = discord.Embed(description=f"`Can't find user: {username}`",color=discord.Color.from_rgb(0,0,0))
         embed.set_footer(text=f"Took {round(time.time() - startTime,2)}s but couldn't find the user.")
     else:
+        userJson = getUser[0].json()
+        username = userJson['name']
+        uuid = userJson['id']
+        
         embed = discord.Embed(title=username,url=f"https://namemc.com/profile/{username}",color=discord.Color.random())
         embed.add_field(name="UUID",value=f"`{uuid}`",inline=True)
         embed.set_thumbnail(url=getUser[-1])
