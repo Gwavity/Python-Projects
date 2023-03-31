33b390ee0392de48bf8d97cb443f91804a1ce76f
Alexander <72534647+Gwavity@users.noreply.github.com>
2023-03-31 01:44:44 -0400

    remove comments

--- cards/main.py
+++ cards/main.py
@@ -2,7 +2,6 @@ from card import Card
 import player
 from deck import cardDeck
 
-# player = Player()
 deck = cardDeck()
 
 if __name__ == "__main__":
@@ -16,32 +15,10 @@ if __name__ == "__main__":
         except Exception as e:
             if type(e) == ValueError:
                 print("Please enter a number.\n")
-        
-    # newPlayer = player.Player("John")
-
-    # deck.createDeck()
-
-    # print(" ".join(deck.printDeck(deck.deck)) + "\n")
-    # for i,card in enumerate(deck.deck.copy()):
-    #     if i >= 5:
-    #         break
-    #     print(f"Giving {newPlayer.name} {card.toString()}")
-    #     newPlayer.giveCard(card)
-    #     deck.removeCard(card)
     
-    # print(deck.printDeck(deck.deck))
-
-    
-    for i in range(1):
+    for i in range(playerCount):
         player.Player(input("Enter your name: "))
-        # player.Player("Jake")
-        # player.Player("Alex")
-        # player.Player("Mike")
 
-        
-        # player.addPlayer("a")#playerName=input("Enter your name: "))
-
-    # print(player.players)
     deck.createDeck()
     
     currentPlayerIter = 0
@@ -50,12 +27,9 @@ if __name__ == "__main__":
     
     for cards in deck.deck:
         curerntPlayer = list(player.players[currentPlayerIter].keys())[0]
-
-        # if cards.rank == "ACE":
-        #     choice = input(f"Since {curerntPlayer.name} got an ACE, would you like ")
-
         curerntPlayer.giveCard(cards)
         currentPlayerIter += 1
+        
         if currentPlayerIter >= len(list(player.players)):
             currentPlayerIter = 0
     
@@ -66,26 +40,14 @@ if __name__ == "__main__":
     print("")
     deck.clear()
 
-    # print(deck.printDeck(deck.deck))
     for cards in deck.deck:
         curerntPlayer = list(player.players[currentPlayerIter].keys())[0]
-
-        # if cards.rank == "ACE":
-        #     choice = input(f"Since {curerntPlayer.name} got an ACE, would you like ")
-
         curerntPlayer.giveCard(cards)
         currentPlayerIter += 1
+        
         if currentPlayerIter >= len(list(player.players)):
             currentPlayerIter = 0
     
     for _ in player.players:
         for k,v in _.items():
             print(f"{k.name}: {', '.join(deck.printDeck(v))}")
-        
-
-    
-
-        # print(_.cards)
-    
-    # for _ in
-    # for _ in len()
