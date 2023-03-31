d235c68872687975a2398102224282f490be1217
Alexander <72534647+Gwavity@users.noreply.github.com>
2023-03-31 01:45:29 -0400

    remove comments one more time

--- cards/player.py
+++ cards/player.py
@@ -8,17 +8,6 @@ class Player:
         self.cards = []
         self.name = name
         players.append({self:self.cards})
-
-    # def addPlayer(self,name):
-    #     players.append({name:[]})
-        # print(players)
     
     def giveCard(self,card):
         self.cards.append(card)
-        # cardRemoval = cardDeck().removeCard(card)
-        # print(cardRemoval)
-
-    # def printDeck(self):
-    #     # for _ in self.cards:
-    #     #     print(_.toString())
-    #     return [card.toString() for card in self.cards]
